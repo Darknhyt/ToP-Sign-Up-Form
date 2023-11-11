13838f2aee59548407151224991be535a371ed8b
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2023-11-11 12:05:08 -0400

    Adding responsive events

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
+const light = document.getElementById("color");
+const psw2 = document.getElementById("psw2")
+const psw1 = document.getElementById("psw1");
+const inputs = document.querySelectorAll("input");
 let darkmode = false;
 
-document.getElementById("color").addEventListener("click", () => {
-    if (darkmode) {
-        document.body.style.backgroundColor = "#000";
-        document.body.style.color = "#FFF";
-        darkmode = false;
-    } else {
-        document.body.style.backgroundColor = "#FFF";
-        document.body.style.color = "#000";
-        darkmode = true;
+init();
+function init(){
+    psw1.addEventListener("input", validate);
+    psw2.addEventListener("input", validate);
+    light.addEventListener("click", () => {
+        if (darkmode) {
+            document.body.style.backgroundColor = "#000";
+            document.body.style.color = "#FFF";
+            darkmode = false;
+        } else {
+            document.body.style.backgroundColor = "#FFF";
+            document.body.style.color = "#000";
+            darkmode = true;
+        }
+    });
+    console.log(inputs);
+    inputs.forEach((input)=>{
+        input.addEventListener("input",()=>{
+            if(input.validity.valid){
+                input.style.borderColor = "lime";
+            }else{
+                input.style.borderColor = "red";
+            }
+        })
+    })
+       
+    
+}
+
+function validate() {
+    if (psw1.value.length > 7 && psw2.value.length > 7) {
+        if (psw1.value == psw2.value) {
+            psw1.style.borderColor = "lime";
+            psw2.style.borderColor = "lime";
+        } else {
+            psw1.style.borderColor = "red";
+            psw2.style.borderColor = "red";
+        }
     }
-})
+}
