c154f0ea6ff2e0ba03ffe09e9effab87617338e1
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2023-11-11 15:02:46 -0400

    Adding contextual text

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const light = document.getElementById("color");
 const psw2 = document.getElementById("psw2")
 const psw1 = document.getElementById("psw1");
 const inputs = document.querySelectorAll("input");
+const submit = document.getElementById("btn");
 let darkmode = false;
 
 init();
 function init(){
-    psw1.addEventListener("input", validate);
-    psw2.addEventListener("input", validate);
+    psw1.addEventListener("input", validatePass);
+    psw2.addEventListener("input", validatePass);
+    submit.addEventListener("click",verifyInputs);
     light.addEventListener("click", () => {
         if (darkmode) {
             document.body.style.backgroundColor = "#000";
@@ -19,21 +21,25 @@ function init(){
             darkmode = true;
         }
     });
-    console.log(inputs);
     inputs.forEach((input)=>{
         input.addEventListener("input",()=>{
             if(input.validity.valid){
                 input.style.borderColor = "lime";
-            }else{
+            } else if (input.value.length == 0){
+                input.style.borderColor = "#060";
+            } else {
                 input.style.borderColor = "red";
             }
         })
+        input.addEventListener("keyup",(e)=>{
+            if(e.key == "Enter"){
+                verifyInputs();
+            }
+        })
     })
-       
-    
 }
 
-function validate() {
+function validatePass() {
     if (psw1.value.length > 7 && psw2.value.length > 7) {
         if (psw1.value == psw2.value) {
             psw1.style.borderColor = "lime";
@@ -43,4 +49,17 @@ function validate() {
             psw2.style.borderColor = "red";
         }
     }
+}
+
+function verifyInputs(){
+    let valid = true
+    let errors = "";
+    inputs.forEach((i)=>{
+        if(!i.validity.valid){
+            i.style.borderColor = "red";
+            valid = false;
+            errors += "\n" + i.name.toUpperCase();
+        }
+    })
+    valid? alert("Success..."):alert("Invalid inputs in:"+errors);
 }
